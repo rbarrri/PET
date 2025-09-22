b60d0cff6022a77e917f5a61332135c98b4a4557
ryanbarreto1 <60881745+ryanbarreto1@users.noreply.github.com>
2025-09-21 22:17:59 -0400

    Added add statement button

--- app.py
+++ app.py
@@ -32,29 +32,38 @@ class dbStatement(db.Model):
 def index():
     # Get the csv data a way that wont readd all transactions to the database each time the web page is refreshed
     # get data from csv
+    qStatement = dbStatement.query.order_by(dbStatement.posted_date).all()
+    return render_template('index.html', inStatement = qStatement)
+
+
+@app.route('/add', methods=['GET'])
+def add():
     dfTrans =  pd.read_csv('C:/Users/ryang/OneDrive/Documents/PET/2025-09-04_transaction_download.csv')
-    
+    string_cols = dfTrans.select_dtypes(include="object").columns
+    dfTrans[string_cols] = dfTrans[string_cols].fillna("")
+
     len = dfTrans.index # amount of rows
     for i in len:
-        # create the db object
+        # create the db object for each new trans
         new_trans = dbStatement()
         # assign the cvs values
         print(new_trans)
         new_trans.paid = False
         print(new_trans.paid)
+
         new_trans.trans_date = dfTrans.loc[i, "Transaction Date"]
         print(new_trans.trans_date)
+
         new_trans.posted_date = dfTrans.loc[i, "Posted Date"]
         print(new_trans.posted_date)
+
         new_trans.card_num = dfTrans.loc[i, "Card No."].item()
         print(new_trans.card_num)
+        
         new_trans.description = dfTrans.loc[i, "Description"]
         print(new_trans.description)
 
-        if np.isnan(pd.to_numeric(dfTrans.loc[i, "Category"], errors='coerce')):
-            new_trans.category = ""
-        else:
-            new_trans.category = dfTrans.loc[i, "Category"]
+        new_trans.category = dfTrans.loc[i, "Category"]
         print(new_trans.category)
 
         if np.isnan(dfTrans.loc[i, "Debit"]):
@@ -74,30 +83,12 @@ def index():
         db.session.add(new_trans)
         db.session.commit()
             
-
         #except:
         #    return 'There was an error adding a transaction'
 
         # add each row column to the database
 
-    qStatement = dbStatement.query.order_by(dbStatement.posted_date).all()
-    return render_template('index.html', inStatement = qStatement)
-    #for loop to populate database for each data frame row
-    #pass the result of the query to data base to the html
-
-    #pdStatement = Statement()
-    #return render_template('index.html', inStatement = pdStatement)
-@app.route('/add')
-def add(id):
-    # To do: update to add statement here
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting task: ' + id
+    return redirect('/')
     
 if __name__ == "__main__":
     app.run(debug=True)
